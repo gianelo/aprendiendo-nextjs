64b55d3582d5dbe07a7c27580d57b16a0dd5f5d5
Gian Barboza <gbarboza@hablax.com>
2024-06-29 07:46:43 -0500

    second commit

--- nextjs-dashboard/app/ui/fonts.ts
+++
@@ -1,5 +0,0 @@
-import { Inter, Stardos_Stencil, Lusitana } from 'next/font/google'
-
-export const inter = Inter({subsets: ['latin']});
-export const stardosStencil = Stardos_Stencil({ weight:['400', '700'], subsets: ['latin']});
-export const lusitana = Lusitana({ weight:['400', '700'], subsets: ['latin']});
